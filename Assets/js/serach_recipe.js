707fcabcd16c1e89ded0ffb8e7c481d500bc2169
Udayasooriyan Prabudeva <st20260583@outlook.cardiffmet.ac.uk>
2024-02-07 03:57:25 +0530

    updated serach_recipe

--- Assets/js/serach_recipe.js
+++ Assets/js/serach_recipe.js
@@ -45,3 +45,30 @@ function showMealRecipe(mealId) {
         })
         .catch(error => console.error('Error fetching meal recipe:', error));
 }
+
+
+// Function to show meal recipe modal
+function showMealRecipe(mealId) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+        .then(response => response.json())
+        .then(data => {
+            const meal = data.meals[0];
+            const modalBody = document.getElementById('recipeModalBody');
+            const youtubeButton = document.getElementById('youtubeButton');
+            
+            // Populate modal with recipe details
+            modalBody.innerHTML = `
+                <img src="${meal.strMealThumb}" class="img-fluid mb-3" alt="${meal.strMeal}">
+                <h5>${meal.strMeal}</h5>
+                <p>${meal.strInstructions}</p>
+            `;
+            
+            // Set YouTube button href to recipe video URL
+            youtubeButton.href = meal.strYoutube;
+            
+            // Show modal
+            const recipeModal = new bootstrap.Modal(document.getElementById('recipeModal'));
+            recipeModal.show();
+        })
+        .catch(error => console.error('Error fetching meal recipe:', error));
+}
